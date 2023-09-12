c8b090dd446eee268e352138be38aefb99d85041
Zynab399 <parkrevian1997@gmail.com>
2023-09-12 08:43:06 -0700

    adding readme file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* more headers goes there */
+/**
+* main - written by soma
+*
+* Return: Always
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "unistd.h"
+/**
+* _putchar - write the character c to stdout
+* @c: the character to print
+*
+* Return: On success 1
+*/
+
