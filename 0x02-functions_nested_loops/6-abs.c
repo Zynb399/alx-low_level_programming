b1fcfddd6d9028083542b2e4cc519f388fa390ff
Zynab399 <parkrevian1997@gmail.com>
2023-09-12 12:47:07 -0700

    first 6 tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @intt: the integer to be computed
+ * Return: the absloute value
+ *
+ */
+int _abs(int)
+{
+	int t;
+
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -6,4 +6,7 @@
 *
 * Return: On success 1
 */
-
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
